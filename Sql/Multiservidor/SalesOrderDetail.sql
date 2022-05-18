18260e9ba1cd3a28f64a2dc3c8416bfa8fcee0fd
Alejandro Granados <81429994+aledelorbe@users.noreply.github.com>
2022-05-17 22:15:11 -0500

    sp's orderDetail

--- Sql/Multiservidor/SalesOrderDetail.sql
+++ Sql/Multiservidor/SalesOrderDetail.sql
@@ -143,3 +143,75 @@ end
 go
 
 exec sp_ValidarInserccionSalesOrderDetail 1,11
+
+
+-- Insertar 
+-- porque no jala la insercion remota si, si jala fuera de el sp. 
+create or alter procedure sp_BuscaSalesOrderDetail
+	@SalesOrderID int, @ProductID int, @cantidad smallint
+as begin 
+	BEGIN TRY
+		BEGIN TRANSACTION
+
+			Declare @tipo_oferta int, @precio money, @descuento smallmoney, @total numeric(38,6)
+			create table #tablaPrecio( preci money )
+
+			DECLARE @TSQL varchar(8000), @productNo VARCHAR(100)
+			SELECT  @productNo = CONVERT(varchar(100), @ProductID)
+			SELECT  @TSQL = 'select * from openquery(MYSQL,''select ListPrice from AdventureWorks2019.product
+							WHERE ProductID = ''''' + @productNo + ''''''')'
+			
+			insert into #tablaPrecio exec(@tsql)
+
+			set @precio = ( select * from #tablaPrecio )
+
+					
+			IF EXISTS (select * from  SERVIDOR2.SALES.[Sales].[SpecialOfferProduct] where ProductID = @ProductID )
+					BEGIN
+					-- insertarlo pero aplicando su respectivo descuento.
+					
+					set @tipo_oferta = ( select top 1 SpecialOfferID from  SERVIDOR2.SALES.[Sales].[SpecialOfferProduct] 
+																	  where ProductID = @ProductID )
+
+    				set @descuento = ( select DiscountPct from  SERVIDOR2.SALES.[Sales].[SpecialOffer]
+						     			where SpecialOfferID = @tipo_oferta )
+									
+					set @total = ( @cantidad*@precio*(1-@descuento) )
+						
+					SELECT @SalesOrderID, @cantidad, @ProductID, @tipo_oferta, @precio, @descuento, @total, NEWID(), getdate()
+					--INSERT INTO SERVIDOR2.SALES.[Sales].[SalesOrderDetail] (SalesOrderID, [OrderQty], [ProductID],
+					--	[SpecialOfferID], [UnitPrice], UnitPriceDiscount, [LineTotal], rowguid, ModifiedDate)
+					--VALUES (@SalesOrderID, @cantidad, @ProductID, @tipo_oferta, @precio, @descuento, @total, NEWID(), getdate())	
+					
+					END
+			ELSE
+				    BEGIN
+					--PRINT N'Esta producto no tiene algun tipo de oferta'; 
+						-- insertarlo normal pero poniendo specialOfferID igual a 1
+					set @tipo_oferta = 1
+					set @descuento = (select DiscountPct from  SERVIDOR2.SALES.[Sales].[SpecialOffer]
+						     			where SpecialOfferID = @tipo_oferta )			
+					set @total = ( @cantidad*@precio*(1-@descuento) )
+					
+					SELECT @SalesOrderID, @cantidad, @ProductID, @tipo_oferta, @precio, @descuento, @total, NEWID(), getdate()
+
+
+					--INSERT INTO SERVIDOR2.SALES.[Sales].[SalesOrderDetail] (SalesOrderID, [OrderQty], [ProductID],
+					--	[SpecialOfferID], [UnitPrice], UnitPriceDiscount, [LineTotal], rowguid, ModifiedDate)
+					--VALUES (@SalesOrderID, @cantidad, @ProductID, @tipo_oferta, @precio, @descuento, @total, NEWID(), getdate())
+					END
+		COMMIT TRANSACTION
+	END TRY 
+	BEGIN CATCH   
+		ROLLBACK TRANSACTION   
+		RAISERROR ('No se pudo realizar la accion',16,1)  
+	END CATCH
+end
+go
+
+exec sp_BuscaSalesOrderDetail 1811, 517, 2
+
+-- insercion manual
+INSERT INTO SERVIDOR2.SALES.[Sales].[SalesOrderDetail] (SalesOrderID, [OrderQty], [ProductID],
+						[SpecialOfferID], [UnitPrice], UnitPriceDiscount, [LineTotal], rowguid, ModifiedDate)
+VALUES (1811, 1, 797, 1, 45, 0.00, 45, NEWID(), getdate());	
---
+++ Sql/Multiservidor/SalesOrderHeader.sql
@@ -0,0 +1,94 @@
+use AdventureWorks2019
+go
+
+-- RECUPERACION
+create or alter procedure sp_RecuperarSalesOrderHeader
+as begin 
+	BEGIN TRY
+		BEGIN TRANSACTION
+			select * 
+			from SERVIDOR2.SALES.[Sales].[SalesOrderHeader]
+		COMMIT TRANSACTION
+	END TRY 
+	BEGIN CATCH   
+		ROLLBACK TRANSACTION   
+		RAISERROR ('No se pudo realizar la accion',16,1)  
+	END CATCH
+end
+
+exec sp_RecuperarSalesOrderHeader 
+
+
+-- INSERTAR
+
+-- [SalesOrderID]: Es el numero de orden de una venta en salesOrderDetail.
+-- [RevisionNumber]: Se trae de la tabla PurcharseOrderHeader, pero hay que pasar primero por 
+	-- ShipMethod.
+-- [OrderDate]: Se trae de la tabla PurcharseOrderHeader, pero hay que pasar primero por 
+	-- ShipMethod.
+-- [DueDate]: Se trae de la tabla PurcharseOrderDetail, pero hay que pasar primero por 
+	-- PurcharseOrderHeader y ShipMethod.
+-- [ShipDate]: Se trae de la tabla PurcharseOrderHeader, pero hay que pasar primero por 
+	-- ShipMethod.
+-- [Status]: Se trae de la tabla PurcharseOrderHeader, pero hay que pasar primero por 
+	-- ShipMethod.
+-- [OnlineOrderFlag]: Pedirla al usuario.
+-- [SalesOrderNumber]: Es el mismo valor que SalesOrderId pero con el prefijo: SO.
+-- [PurchaseOrderNumber]: Dejarla nula.
+-- [AccountNumber]: Dejarlo nulo.
+-- [CustomerID]: Se solicita al usuario. Se busca en la tabla customer.
+-- [SalesPersonID]: Dejarlo nulo.
+-- [TerritoryID]: Se busca en la tabla customer.
+-- [BillToAddressID]: Ingresar uno aleatorio.
+--[ShipToAddressID]: El mismo que el de arriba.
+--[ShipMethodID]: Se trae de la tabla shipMethod.
+--[CreditCardID]: Se trae de la tabla credtCard, pero hay que pasar por customer, person, 
+	--credit person y credit card.
+--[CreditCardApprovalCode]:
+--[CurrencyRateID]: Dejarlo nulo.
+--[SubTotal]: Es la suma de todos los line total de una orden. 
+--[TaxAmt]: 
+--[Freight]: 
+--[TotalDue]: Es el resultado de Subtotal + TaxAmt + Freight.
+--[Comment]: Dejarlo nulo.
+--[rowguid]: NEWID()
+--[ModifiedDate]: GETDATE()
+
+select * 
+from SERVIDOR2.SALES.[Sales].[SalesOrderHeader]
+where SalesOrderID = 48092
+
+select * 
+from [Purchasing].[ShipMethod]
+
+select *
+from SERVIDOR2.SALES.[Sales].[SalesOrderDetail]
+where SalesOrderID = 48092
+
+
+select * 
+from SERVIDOR2.SALES.[Sales].customer
+
+select * 
+from Person.Person
+where BusinessEntityID = 16162
+
+select * 
+from SERVIDOR2.SALES.[Sales].[SalesTerritory]
+
+select * 
+from SERVIDOR2.SALES.[Sales].[CreditCard]
+where CreditCardID = 12922
+
+select * 
+from SERVIDOR2.SALES.[Sales].[PersonCreditCard]
+where CreditCardID = 12922 -- 16162
+
+
+
+
+UPDATE OPENQUERY(MYSQL,'SELECT SafetyStockLevel FROM AdventureWorks2019.product WHERE ProductID = 1') SET SafetyStockLevel = 5
+
+update AdventureWorks2019.product
+set SafetyStockLevel = 20
+where ProductID=1
