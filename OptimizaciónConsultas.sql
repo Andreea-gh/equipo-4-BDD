7155f70342062e006824b9ab63ef4ded7a722777
Carloscrm2 <cgomez.egarcia@gmail.com>
2022-06-06 22:06:58 -0500

    Se agregaron comentarios sobre los indices creados para la optimizacion de consultas y el archivo pdf donde se muestran capturas de los planes de ejecucion

--- OptimizaciónConsultas.sql
+++ OptimizaciónConsultas.sql
@@ -34,6 +34,21 @@ Listado de consultas a programar para anlizar planes de ejecución
 -- solución 1
 -- Debido a que un paciente se considera como caso positivo en covid si este tiene un valor
 -- ya sea 1, 2 o 3 en el campo clasificacion final, es que se busca a los registros en dicho rango en dicho campo.
+
+-- SOBRE LOS INDICES: Decidimos crear indices agrupados ya que son mas rapidos que los no agrupados, ademas de que los no agrupados
+-- empeoran cuando hay demasiados datos. Creamos un indice sobre la columna ENTIDAD_RES porque sus datos no se repiten, también hicimos
+-- pruebas sobre ENTIDAD_UM, MUNICIPIO_RES pero no notamos una diferencia en el rendimiento por lo menos en los planes de ejeucion no vimos
+-- una diferencia en porcentaje
+
+-- Creamos un indice agrupado sobre la columna ENTIDAD_RES porque los datos en dicha columna
+-- no se repiten ademas de que en las consultas el order by recae sobre el atributo ENTIDAD_RES
+CREATE CLUSTERED INDEX IX_ENTIDAD_RES ON dbo.copiadatoscovid(ENTIDAD_RES)
+
+select *
+from dbo.copiadatoscovid
+where CLASIFICACION_FINAL between 1 and 3
+order by ENTIDAD_RES
+
 select *
 from dbo.datoscovid
 where CLASIFICACION_FINAL between 1 and 3
@@ -41,6 +56,12 @@ order by ENTIDAD_RES
 
 -- solución 2
 -- Se realizo de esta otra manera para contabilizar cuantos casos positivos hay por cada entidad.
+select ENTIDAD_RES, count(*) total_confirmado
+from dbo.copiadatoscovid
+where CLASIFICACION_FINAL between 1 and 3
+group by ENTIDAD_RES
+order by ENTIDAD_RES
+
 select ENTIDAD_RES, count(*) total_confirmado
 from dbo.datoscovid
 where CLASIFICACION_FINAL between 1 and 3
@@ -54,6 +75,18 @@ order by ENTIDAD_RES
 -- de 6 en el campo clasificacion final, es que se busca a los registros en dicho valor en dicho campo.
 -- Como no se entiende bien sobre cual entidad se refiere el enunciado, se seleccionan las entidades mas importantes, 
 -- siendo la entidad de unidad medica y entidad residencial.
+
+-- Al igual que en la consulta anterior podemos crear un indice sobre 
+-- ENTIDAD_RES, solo que también podriamos hacerlo sobre ENTIDAD_UM y 
+-- tendría el mismo efecto 
+CREATE CLUSTERED INDEX IX_ENTIDAD_RES ON dbo.copiadatoscovid(ENTIDAD_RES)
+
+select ENTIDAD_UM, ENTIDAD_RES, count(*) total_sospechosos
+from dbo.copiadatoscovid
+where CLASIFICACION_FINAL = 6
+group by ENTIDAD_UM, ENTIDAD_RES
+order by ENTIDAD_UM 
+
 select ENTIDAD_UM, ENTIDAD_RES, count(*) total_sospechosos
 from dbo.datoscovid
 where CLASIFICACION_FINAL = 6
@@ -63,6 +96,11 @@ order by ENTIDAD_UM
 -- Solucion 2
 -- Como muy probablemente la entidad residencial sea la mas importante, es que en esta otra solucion unicamnte se selecciona
 -- este campo y se contabiliza cuantos casos sospeshocos hay por cada entidad residencial.
+select ENTIDAD_RES, count(*) total_sospechosos
+from dbo.copiadatoscovid
+where CLASIFICACION_FINAL = 6
+group by ENTIDAD_RES
+
 select ENTIDAD_RES, count(*) total_sospechosos
 from dbo.datoscovid
 where CLASIFICACION_FINAL = 6
@@ -80,7 +118,21 @@ group by ENTIDAD_RES
 -- Debido a que un paciente se considera como caso positivo en covid si este tiene un valor
 -- ya sea 1, 2 o 3 en el campo clasificacion final, es que se busca a los registros en dicho rango en dicho campo.
 -- Como no se entiende bien sobre cual entidad se refiere el enunciado, se seleccionan las entidades mas importantes, 
--- siendo la entidad de unidad medica y entidad residencial. 
+-- siendo la entidad de unidad medica y entidad residencial.
+
+-- De manera similiar al caso anterior creamos un indice agrupado solo que ahora sobre la columna MUNICIPO_RES
+-- hacerlo sobre las otras columnas de la consulta no tiene una diferencia visible sobre el rendimiento
+CREATE CLUSTERED INDEX IX_MUNICIPIO_RES ON dbo.copiadatoscovid(MUNICIPIO_RES)
+
+Select ENTIDAD_RES, MUNICIPIO_RES, count(*) as reportados, count(case CLASIFICACION_FINAL when 1 then CLASIFICACION_FINAL 
+                                                                  when 2 then CLASIFICACION_FINAL
+																  when 3 then CLASIFICACION_FINAL
+                                          end) as confirmado,
+       count(case CLASIFICACION_FINAL when 6 then CLASIFICACION_FINAL end) as sospechoso
+from dbo.copiadatoscovid
+group by ENTIDAD_RES, MUNICIPIO_RES
+order by ENTIDAD_RES, reportados desc
+
 Select ENTIDAD_RES, MUNICIPIO_RES, count(*) as reportados, count(case CLASIFICACION_FINAL when 1 then CLASIFICACION_FINAL 
                                                                   when 2 then CLASIFICACION_FINAL
 																  when 3 then CLASIFICACION_FINAL
@@ -92,6 +144,18 @@ order by ENTIDAD_RES, reportados desc
 
 -- solución 2
 -- Esta solucion se realizo con ideas totalmente iguales a las anteriores pero utilizando inner join.
+select cc.ENTIDAD_RES, cc.MUNICIPIO_RES, cc.confirmado, cs.sospechoso
+from (select ENTIDAD_RES, MUNICIPIO_RES, count(*) as sospechoso 
+      from dbo.copiadatoscovid where CLASIFICACION_FINAL = 6
+	  group by ENTIDAD_RES, MUNICIPIO_RES
+      ) cs
+inner join
+(select ENTIDAD_RES, MUNICIPIO_RES, count (*) as confirmado 
+ from dbo.copiadatoscovid where CLASIFICACION_FINAL between 1 and 3
+ group by ENTIDAD_RES, MUNICIPIO_RES) cc 
+on cc.ENTIDAD_RES =  cs.ENTIDAD_RES and cs.MUNICIPIO_RES = cc.MUNICIPIO_RES
+order by cc.ENTIDAD_RES, cc.MUNICIPIO_RES
+
 select cc.ENTIDAD_RES, cc.MUNICIPIO_RES, cc.confirmado, cs.sospechoso
 from (select ENTIDAD_RES, MUNICIPIO_RES, count(*) as sospechoso 
       from dbo.datoscovid where CLASIFICACION_FINAL = 6
@@ -117,6 +181,17 @@ order by cc.ENTIDAD_RES, cc.MUNICIPIO_RES
 -- Finalmente se vuelve a contabilizar cuantos muertos hay en casos positivos al covid por cada municipio residencial pero 
 -- condicionando que solo se traiga la mayor cantidad.
 select MUNICIPIO_RES, COUNT(*) as numero_Defunciones
+from dbo.copiadatoscovid
+where CLASIFICACION_FINAL between 1 and 3 and FECHA_DEF != '9999-99-99'
+group by MUNICIPIO_RES 
+having COUNT(*) = ( select max(numero_Defunciones)
+					from (
+						select MUNICIPIO_RES, COUNT(*) as numero_Defunciones
+						from dbo.copiadatoscovid
+						where CLASIFICACION_FINAL between 1 and 3 and FECHA_DEF != '9999-99-99'
+						group by MUNICIPIO_RES ) as aux )
+
+select MUNICIPIO_RES, COUNT(*) as numero_Defunciones
 from dbo.datoscovid
 where CLASIFICACION_FINAL between 1 and 3 and FECHA_DEF != '9999-99-99'
 group by MUNICIPIO_RES 
@@ -126,9 +201,6 @@ having COUNT(*) = ( select max(numero_Defunciones)
 						from dbo.datoscovid
 						where CLASIFICACION_FINAL between 1 and 3 and FECHA_DEF != '9999-99-99'
 						group by MUNICIPIO_RES ) as aux )
-
-
--- Solucion 2
 
 
 -- 5  Determinar por entidad, si de casos sospechosos hay defunciones reportadas asociadas a neumonia.
@@ -142,6 +214,11 @@ having COUNT(*) = ( select max(numero_Defunciones)
 -- Como no se entiende bien sobre cual entidad se refiere el enunciado, se seleccionan las entidades mas importantes, 
 -- siendo la entidad de unidad medica y entidad residencial. De ahi se contabiliza cuantos registros hay por cada entidad.
 select ENTIDAD_UM, ENTIDAD_RES, count(*)
+from dbo.copiadatoscovid
+where CLASIFICACION_FINAL = 6 and FECHA_DEF != '9999-99-99' and NEUMONIA = 1
+group by ENTIDAD_UM, ENTIDAD_RES
+
+select ENTIDAD_UM, ENTIDAD_RES, count(*)
 from dbo.datoscovid
 where CLASIFICACION_FINAL = 6 and FECHA_DEF != '9999-99-99' and NEUMONIA = 1
 group by ENTIDAD_UM, ENTIDAD_RES
@@ -149,6 +226,11 @@ group by ENTIDAD_UM, ENTIDAD_RES
 -- Solucion 2
 -- Como muy probablemente la entidad residencial sea la mas importante, es que en esta otra solucion unicamnte se selecciona
 -- este campo cuantos registros hay por cada entidad residencial.
+select ENTIDAD_RES, count(*)
+from dbo.copiadatoscovid
+where CLASIFICACION_FINAL = 6 and FECHA_DEF != '9999-99-99' and NEUMONIA = 1
+group by ENTIDAD_RES
+
 select ENTIDAD_RES, count(*)
 from dbo.datoscovid
 where CLASIFICACION_FINAL = 6 and FECHA_DEF != '9999-99-99' and NEUMONIA = 1
@@ -172,13 +254,20 @@ Select ENTIDAD_RES, count(case CLASIFICACION_FINAL when 6 then CLASIFICACION_FIN
                                                            when 2 then CLASIFICACION_FINAL
 		               									   when 3 then CLASIFICACION_FINAL
     end) as confirmado, COUNT( case when  FECHA_DEF != '9999-99-99' then FECHA_DEF end ) as total_Defunciones      
-from dbo.datoscovid
+from dbo.copiadatoscovid
 where FECHA_INGRESO between '2020-03-01' and '2020-08-31' or FECHA_INGRESO between '2021-05-01' and '2021-12-31' 
 group by ENTIDAD_RES
 order by ENTIDAD_RES
 
--- Solucion 2
-
+Select ENTIDAD_RES, count(case CLASIFICACION_FINAL when 6 then CLASIFICACION_FINAL end) as sospechoso, 
+	count(*) as reportados, count(case CLASIFICACION_FINAL when 1 then CLASIFICACION_FINAL 
+                                                           when 2 then CLASIFICACION_FINAL
+		               									   when 3 then CLASIFICACION_FINAL
+    end) as confirmado, COUNT( case when  FECHA_DEF != '9999-99-99' then FECHA_DEF end ) as total_Defunciones      
+from dbo.datoscovid
+where FECHA_INGRESO between '2020-03-01' and '2020-08-31' or FECHA_INGRESO between '2021-05-01' and '2021-12-31' 
+group by ENTIDAD_RES
+order by ENTIDAD_RES
 
 -- 7. Listar los 5 municipios con el mayor número de casos confirmados en niños menos de 13 años con alguna comorbilidad reportada y 
 	--cuantos de esos casos fallecieron.
@@ -195,6 +284,20 @@ order by ENTIDAD_RES
 -- anteriormente mencionadas.
 -- Se ordenaron de mayor a menor el numero de casos confirmados por municipio residencial.
 -- Finalmente se selecciono mostrar los primeros 5 registros.
+Select top 5 MUNICIPIO_RES, count(case CLASIFICACION_FINAL when 1 then CLASIFICACION_FINAL 
+                                                     when 2 then CLASIFICACION_FINAL
+						      						 when 3 then CLASIFICACION_FINAL
+                                          end) as confirmado, 
+					  COUNT( case when  FECHA_DEF != '9999-99-99' then FECHA_DEF end ) as total_Defunciones  
+from dbo.copiadatoscovid
+where EDAD < 13  and ID_REGISTRO in ( select ID_REGISTRO
+									  from dbo.copiadatoscovid
+									  where EDAD < 13 and ( (NEUMONIA = 1 and DIABETES = 1) or (NEUMONIA = 1 and ASMA = 1) 
+														or (NEUMONIA = 1 and HIPERTENSION = 1) or (DIABETES = 1 and ASMA = 1) 
+														or (DIABETES = 1 and HIPERTENSION = 1) or (ASMA = 1 and HIPERTENSION = 1)) )
+group by MUNICIPIO_RES
+order by confirmado desc
+
 Select top 5 MUNICIPIO_RES, count(case CLASIFICACION_FINAL when 1 then CLASIFICACION_FINAL 
                                                      when 2 then CLASIFICACION_FINAL
 						      						 when 3 then CLASIFICACION_FINAL
@@ -221,6 +324,22 @@ order by confirmado desc
 -- una cantidad mayor o ya sea el año 2021 o 2022.
 Declare @cant2020 int, @cant2021 int, @cant2022 int
 set @cant2020 = ( Select COUNT( case when  FECHA_DEF like '2020-%' then FECHA_DEF end ) as total_Defunciones  
+				  from dbo.copiadatoscovid
+				  where EDAD < 18 ) -- 1776
+set @cant2021 = ( Select COUNT( case when  FECHA_DEF like '2021-%' then FECHA_DEF end ) as total_Defunciones  
+				  from dbo.copiadatoscovid
+				  where EDAD < 18 ) -- 1636
+set @cant2022 = ( Select COUNT( case when  FECHA_DEF like '2022-%' then FECHA_DEF end ) as total_Defunciones  
+				  from dbo.copiadatoscovid
+				  where EDAD < 18 ) -- 314
+
+IF @cant2020 > @cant2021 and @cant2020 > @cant2022
+	print 'En el año 2020 hubo una mayor cantidad de defunciones en personas menores de edad que en el año 2021 y 2022'
+ELSE
+	print 'En el año 2020 NO hubo una mayor cantidad de defunciones en personas menores de edad que en el año 2021 y 2022'
+
+	Declare @cant2020 int, @cant2021 int, @cant2022 int
+set @cant2020 = ( Select COUNT( case when  FECHA_DEF like '2020-%' then FECHA_DEF end ) as total_Defunciones  
 				  from dbo.datoscovid
 				  where EDAD < 18 ) -- 1776
 set @cant2021 = ( Select COUNT( case when  FECHA_DEF like '2021-%' then FECHA_DEF end ) as total_Defunciones  
@@ -241,6 +360,18 @@ ELSE
 -- se agruparon estos datos en una sola tabla con union all, para que un usuario pueda visualizar cuantas defunciones hubo por 
 -- año y sea este mismo quien determine la respuesta a la pregunta.
 Select 2020 as Año, COUNT( case when  FECHA_DEF like '2020-%' then FECHA_DEF end ) as total_Defunciones  
+from dbo.copiadatoscovid
+where EDAD < 18 
+UNION ALL 
+Select 2021 as Año, COUNT( case when  FECHA_DEF like '2021-%' then FECHA_DEF end ) as total_Defunciones  
+from dbo.copiadatoscovid
+where EDAD < 18 
+UNION ALL
+Select 2022 as Año, COUNT( case when  FECHA_DEF like '2022-%' then FECHA_DEF end ) as total_Defunciones  
+from dbo.copiadatoscovid
+where EDAD < 18
+
+Select 2020 as Año, COUNT( case when  FECHA_DEF like '2020-%' then FECHA_DEF end ) as total_Defunciones  
 from dbo.datoscovid
 where EDAD < 18 
 UNION ALL 
@@ -250,7 +381,7 @@ where EDAD < 18
 UNION ALL
 Select 2022 as Año, COUNT( case when  FECHA_DEF like '2022-%' then FECHA_DEF end ) as total_Defunciones  
 from dbo.datoscovid
-where EDAD < 18 
+where EDAD < 18
 
 -- 9. Determinar si en el año 2021 hay un pocentaje mayor al 60 de casos reportados que son confirmados por estudios de laboratorio 
 	--en comparación al año 2020.
@@ -280,9 +411,50 @@ if @porcentaje2021 > @porcentaje2020
 else
 	print 'En el año 2020 hay un pocentaje mayor de casos reportados que son confirmados por estudios de laboratorio en comparación al año 2021.'
 
+Declare @porcentaje2021 int, @porcentaje2020 int
+set @porcentaje2021 = (	select aux.Confirmados_Laboratorio*100/aux.Reportados as Confirmados_Laboratorio
+						from (
+							Select count(*) as Reportados, COUNT( case when CLASIFICACION_FINAL between 1 and 3 then CLASIFICACION_FINAL end ) as Confirmados, 
+														   COUNT( case when CLASIFICACION_FINAL = 3 then CLASIFICACION_FINAL end ) as Confirmados_Laboratorio
+							from dbo.copiadatoscovid
+							where FECHA_INGRESO like '2021-%' ) as aux ) -- 26%
+
+set @porcentaje2020 = (	select aux.Confirmados_Laboratorio*100/aux.Reportados as Confirmados_Laboratorio
+						from (
+							Select count(*) as Reportados, COUNT( case when CLASIFICACION_FINAL between 1 and 3 then CLASIFICACION_FINAL end ) as Confirmados, 
+														   COUNT( case when CLASIFICACION_FINAL = 3 then CLASIFICACION_FINAL end ) as Confirmados_Laboratorio
+							from dbo.copiadatoscovid
+							where FECHA_INGRESO like '2020-%' ) as aux ) -- 38%
+
+if @porcentaje2021 > @porcentaje2020
+	print 'En el año 2021 hay un pocentaje mayor de casos reportados que son confirmados por estudios de laboratorio en comparación al año 2020.'
+else
+	print 'En el año 2020 hay un pocentaje mayor de casos reportados que son confirmados por estudios de laboratorio en comparación al año 2021.'
+
 
 -- Solucion 2	
 -- Se realizo un analisis igual pero utilizando subconsultas.
+Declare @porcentaje2021 int, @porcentaje2020 int
+
+set @porcentaje2021 = ( Select ( Select count(*)
+								from dbo.copiadatoscovid
+								where FECHA_INGRESO like '2021-%' and CLASIFICACION_FINAL = 3 ) *100/ COUNT(*) as Confirmados_Laboratorio
+						from dbo.copiadatoscovid
+						where FECHA_INGRESO like '2021-%' )
+
+set @porcentaje2020 = ( Select ( Select count(*)
+								from dbo.copiadatoscovid
+								where FECHA_INGRESO like '2020-%' and CLASIFICACION_FINAL = 3 ) *100/ COUNT(*) as Confirmados_Laboratorio
+						from dbo.copiadatoscovid
+						where FECHA_INGRESO like '2020-%' )
+
+if @porcentaje2021 > @porcentaje2020
+	print 'En el año 2021 hay un pocentaje mayor de casos reportados que son confirmados por estudios de laboratorio en comparación al año 2020.'
+else
+	print 'En el año 2020 hay un pocentaje mayor de casos reportados que son confirmados por estudios de laboratorio en comparación al año 2021.'
+
+
+
 Declare @porcentaje2021 int, @porcentaje2020 int
 
 set @porcentaje2021 = ( Select ( Select count(*)
@@ -314,6 +486,42 @@ else
 -- Se busco la cantidad mayor de personas vivos.
 -- Se volvio a buscar de nuevo a la cantidad de pacientes vivos en los rangos de edad mencionados por el enunciado pero condicionando
 -- que solo mostrara la cantidad que sea igual a la cantidad mayor de pacientes vivos.
+SELECT *
+from
+	( select 'menor de edad' as Rango_Edad, COUNT(*) as Recuperados
+	from dbo.copiadatoscovid
+	where EDAD < 18 and FECHA_DEF = '9999-99-99'
+	UNION ALL
+	select '19 a 40' as Rango_Edad, COUNT(*) as Recuperados
+	from dbo.copiadatoscovid
+	where EDAD between 19 and 40 and FECHA_DEF = '9999-99-99'
+	UNION ALL
+	select '40 a 60' as Rango_Edad, COUNT(*) as Recuperados
+	from dbo.copiadatoscovid
+	where EDAD between 40 and 60 and FECHA_DEF = '9999-99-99'
+	UNION ALL
+	select 'mayor a 60' as Rango_Edad, COUNT(*) as Recuperados
+	from dbo.copiadatoscovid
+	where EDAD > 60 and FECHA_DEF = '9999-99-99' ) as aux
+where aux.Recuperados = (  SELECT MAX(aux2.Recuperados)
+						from
+							( select 'menor de edad' as Rango_Edad, COUNT(*) as Recuperados
+							from dbo.datoscovid
+							where EDAD < 18 and FECHA_DEF = '9999-99-99'
+							UNION ALL
+							select '19 a 40' as Rango_Edad, COUNT(*) as Recuperados
+							from dbo.copiadatoscovid
+							where EDAD between 19 and 40 and FECHA_DEF = '9999-99-99'
+							UNION ALL
+							select '40 a 60' as Rango_Edad, COUNT(*) as Recuperados
+							from dbo.datoscovid
+							where EDAD between 40 and 60 and FECHA_DEF = '9999-99-99'
+							UNION ALL
+							select 'mayor a 60' as Rango_Edad, COUNT(*) as Recuperados
+							from dbo.copiadatoscovid
+							where EDAD > 60 and FECHA_DEF = '9999-99-99' ) as aux2 )
+
+
 SELECT *
 from
 	( select 'menor de edad' as Rango_Edad, COUNT(*) as Recuperados
